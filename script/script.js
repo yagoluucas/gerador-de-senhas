2e86075ad2b1dd9cfa614add28f435553fc7aa25
yagoluucas <yago543@gmail.com>
2024-01-04 09:48:24 -0300

    feito HTML principal e adicionado a funcao que seleciona a opcao de senha escolhida

---
+++ script/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btnGerarSenha = document.querySelector('input[type=button]')
+    const paragrafoSenha = document.querySelector('.js-paragrafo-senha')
+    const select = document.querySelector('.js-opcoes')
+    btnGerarSenha.addEventListener('click', () => {
+        
+    })
+})
