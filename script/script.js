f7dfaf89c6afdf698215d39b4dc08f78780515a1
yagoluucas <yago543@gmail.com>
2024-01-04 21:16:30 -0300

    adicionado função que faz uma senha forte

--- script/script.js
+++ script/script.js
@@ -2,12 +2,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const btnGerarSenha = document.querySelector('input[type=button]')
     const paragrafoSenha = document.querySelector('.js-paragrafo-senha')
     const select = document.querySelector('.js-opcoes')
-    const letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&"
+    const letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const caracteresEspeciais = "!@#$%&*£¢"
+
     function senhaFraca() {
-        let senha = (Math.random() * 10).toFixed();
-        for(let i = 0; i < 2; i++) {
-            let letra = letras.charAt(Math.floor(Math.random() * 26))
-            let numeroAleatorio = (Math.random() * 20).toFixed();
+        let senha = (Math.random() * 9).toFixed();
+        for(let i = 0; i < 3; i++) {
+            let letra = letras.charAt(Math.ceil(Math.random() * 26))
+            let numeroAleatorio = (Math.random() * 9).toFixed();
             senha += numeroAleatorio;
             senha += letra;
         }
@@ -17,14 +19,28 @@ window.addEventListener('DOMContentLoaded', () => {
     function senhaMedia() {
         let senha = letras.charAt(Math.floor(Math.random() * 26));
         for(let i = 0; i < 2; i++) {
-            let letraMinuscula = letras.charAt(Math.floor(Math.random() * 26));
-            let numeroAleatorio = (Math.random() * 20).toFixed();
-            let letraMaiuscula = letras.charAt(Math.floor(Math.random() * (52 - 26)) + 26);
+            let numeroAleatorio = (Math.random() * 9).toFixed();
+            let letraMinuscula = letras.charAt(Math.ceil(Math.random() * 26));
+            let letraMaiuscula = letras.charAt(Math.ceil(Math.random() * (52 - 26)) + 26);
             senha += numeroAleatorio;
             senha += letraMinuscula;
             senha += letraMaiuscula;
         }
+        return senha;
+    }
 
+    function senhaForte() {
+        let senha = caracteresEspeciais.charAt(Math.ceil(Math.random() * caracteresEspeciais.length - 1));
+        for(let i = 0; i < 2; i++) {
+           let numeroAleatorio = Math.ceil(Math.random() * 9);
+           let caracterEspecial = caracteresEspeciais.charAt(Math.ceil(Math.random() * caracteresEspeciais.length - 1));
+           let letraMaiuscula = letras.charAt(Math.ceil(Math.random() * (52 - 26)) + 26);
+           let letraMinuscula = letras.charAt(Math.ceil(Math.random() * 26));
+           senha += numeroAleatorio;
+           senha += letraMaiuscula;
+           senha += caracterEspecial;
+           senha += letraMinuscula;
+        }
         return senha;
     }
 
@@ -35,7 +51,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 paragrafoSenha.textContent = senhaFraca();
                 break;
             case "Media":
-                paragrafoSenha.textContent =  senhaMedia();
+                paragrafoSenha.textContent = senhaMedia();
+                break;
+            case "Forte":
+                paragrafoSenha.textContent = senhaForte();
         }
     })
 })
