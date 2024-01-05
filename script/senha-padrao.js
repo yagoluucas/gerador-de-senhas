098ad415b7259e6ac80bdd4846abf17fdae4f6ba
yagoluucas <yago543@gmail.com>
2024-01-04 21:35:50 -0300

    separado os arquivos de script entre modulos

---
+++ script/senha-padrao.js
@@ -0,0 +1,62 @@
+export function senhaPadrao() {
+    window.addEventListener('DOMContentLoaded', () => {
+        const btnGerarSenha = document.querySelector('input[type=button]')
+        const paragrafoSenha = document.querySelector('.js-paragrafo-senha')
+        const select = document.querySelector('.js-opcoes')
+        const letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        const caracteresEspeciais = "!@#$%&*£¢"
+    
+        function senhaFraca() {
+            let senha = (Math.random() * 9).toFixed();
+            for(let i = 0; i < 3; i++) {
+                let letra = letras.charAt(Math.ceil(Math.random() * 26))
+                let numeroAleatorio = (Math.random() * 9).toFixed();
+                senha += numeroAleatorio;
+                senha += letra;
+            }
+            return senha;
+        }
+    
+        function senhaMedia() {
+            let senha = letras.charAt(Math.floor(Math.random() * 26));
+            for(let i = 0; i < 2; i++) {
+                let numeroAleatorio = (Math.random() * 9).toFixed();
+                let letraMinuscula = letras.charAt(Math.ceil(Math.random() * 26));
+                let letraMaiuscula = letras.charAt(Math.ceil(Math.random() * (52 - 26)) + 26);
+                senha += numeroAleatorio;
+                senha += letraMinuscula;
+                senha += letraMaiuscula;
+            }
+            return senha;
+        }
+    
+        function senhaForte() {
+            let senha = caracteresEspeciais.charAt(Math.ceil(Math.random() * caracteresEspeciais.length - 1));
+            for(let i = 0; i < 2; i++) {
+               let numeroAleatorio = Math.ceil(Math.random() * 9);
+               let caracterEspecial = caracteresEspeciais.charAt(Math.ceil(Math.random() * caracteresEspeciais.length - 1));
+               let letraMaiuscula = letras.charAt(Math.ceil(Math.random() * (52 - 26)) + 26);
+               let letraMinuscula = letras.charAt(Math.ceil(Math.random() * 26));
+               senha += numeroAleatorio;
+               senha += letraMaiuscula;
+               senha += caracterEspecial;
+               senha += letraMinuscula;
+            }
+            return senha;
+        }
+    
+        btnGerarSenha.addEventListener('click', () => {
+            let opcaoEscolhida = select.value
+            switch (opcaoEscolhida) {
+                case "Fraca":
+                    paragrafoSenha.textContent = senhaFraca();
+                    break;
+                case "Media":
+                    paragrafoSenha.textContent = senhaMedia();
+                    break;
+                case "Forte":
+                    paragrafoSenha.textContent = senhaForte();
+            }
+        })
+    })
+}
